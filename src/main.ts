e648d8509c34958d56d9d4b5af246be6d52f18bf
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-11-20 00:03:17 -0300

    generate token jwt private and public keys

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { Env } from './env'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3333)
 
 	const configService = app.get<ConfigService<Env, true>>(ConfigService)
 	const port = configService.get('PORT', { infer: true })
