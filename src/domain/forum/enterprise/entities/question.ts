38911fcfa86e73adf18168544dbddf6df43dc281
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-11-21 22:46:08 -0300

    add: new schema prisma

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -8,7 +8,7 @@ import { QuestionBestAnswerChosenEvent } from '../events/question-best-answer-ch
 
 export interface QuestionProps {
 	authorId: UniqueEntityId
-	bestAnswerId?: UniqueEntityId
+	bestAnswerId?: UniqueEntityId | null
 	title: string
 	slug: Slug
 	content: string
@@ -80,7 +80,7 @@ export class Question extends AggregateRoot<QuestionProps> {
 		this.touch()
 	}
 
-	set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+	set bestAnswerId(bestAnswerId: UniqueEntityId | undefined | null) {
 		if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
 			this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
 		}
