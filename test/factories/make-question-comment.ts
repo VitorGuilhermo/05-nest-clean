81696cc3336afef419808aa86b1d3400987ce9b0
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-11-26 22:38:16 -0300

    add: more factories for tests e2e

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -2,6 +2,9 @@ import { faker } from '@faker-js/faker'
 
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { QuestionComment, QuestionCommentProps } from '@/domain/forum/enterprise/entities/question-comment'
+import { PrismaQuestionCommentMapper } from '@/infra/database/prisma/mappers/prisma-question-comment-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { Injectable } from '@nestjs/common'
 
 export function makeQuestionComment(override: Partial<QuestionCommentProps> = {}, id?: UniqueEntityId) {
 	const questionComment = QuestionComment.create({
@@ -12,4 +15,19 @@ export function makeQuestionComment(override: Partial<QuestionCommentProps> = {}
 	}, id)
 
 	return questionComment
-}   
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+	constructor(private prisma: PrismaService) { }
+
+	async makePrismaQuestionComment(data: Partial<QuestionCommentProps> = {}): Promise<QuestionComment> {
+		const questionComment = makeQuestionComment(data)
+
+		await this.prisma.comment.create({
+			data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+		})
+
+		return questionComment
+	}
+}
