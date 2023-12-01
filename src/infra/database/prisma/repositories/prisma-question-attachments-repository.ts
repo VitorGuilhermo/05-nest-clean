378912586ce327e9dc8530f219f351e84b5e6167
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-12-01 13:30:11 -0300

    add: persist attachments in bd

--- src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
@@ -8,6 +8,16 @@ import { PrismaService } from '../prisma.service'
 export class PrismaQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
 	constructor(private prisma: PrismaService) { }
 
+	async createMany(attachments: QuestionAttachment[]): Promise<void> {
+		if (attachments.length === 0) {
+			return
+		}
+
+		const data = PrismaQuestionAttachmentMapper.toPrismaUpdateMany(attachments)
+
+		await this.prisma.attachment.updateMany(data)
+	}
+
 	async findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]> {
 		const questionAttachments = await this.prisma.attachment.findMany({
 			where: {
@@ -22,6 +32,24 @@ export class PrismaQuestionAttachmentsRepository implements QuestionAttachmentsR
 		await this.prisma.attachment.deleteMany({
 			where: {
 				questionId,
+			},
+		})
+	}
+
+	async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+		if (attachments.length === 0) {
+			return
+		}
+
+		const attachmentIds = attachments.map((attachment) => {
+			return attachment.id.toString()
+		})
+
+		await this.prisma.attachment.deleteMany({
+			where: {
+				id: {
+					in: attachmentIds,
+				},
 			},
 		})
 	}
