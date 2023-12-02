daf792a3baf6e42b47ae153e4964050cb283ad05
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-12-02 00:35:19 -0300

    add: comment with author controller

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Controller, Get, Param,Query } from '@nestjs/commo
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { z } from 'zod'
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
-import { CommentPresenter } from '../presenters/comment-presenter'
+import { CommentWithAuthorPresenter } from '../presenters/comment-with-author-presenter'
 
 const pageQueryParamSchema = z
 	.string()
@@ -30,8 +30,8 @@ export class FetchQuestionCommentsController {
 			throw new BadRequestException()
 		}
 
-		const questionComments = result.value.comments
+		const comments = result.value.comments
 
-		return { comments: questionComments.map(CommentPresenter.toHTTP) }
+		return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
 	}
 }
